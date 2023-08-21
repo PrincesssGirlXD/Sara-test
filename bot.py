2cecf59e8bd0520f0975c7c393bc838e14d8a0ef
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2023-08-21 18:40:48 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+
+bot = Client(
+  "uhdhdhhd",
+  api_id=18770647,
+  api_hash="ed11b8af8b51418dbac60b456d1429a7",
+  bot_token="6281251739:AAFGDxuibD-teVFF_wzOTc41dgJtE6kJje4"
+)
+@bot.on_message(filters.command("start"))
+async def starts(client , msg):
+  await message.reply_text("Alpha Server Working Fine")
+
+bot.run()
+  
